d8b8b9bb4581f46d2690711439d656332476c641
The-Mister-dev <ktgktg225@gmail.com>
2025-04-15 20:30:01 +0000

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let typed = new Typed("#typed-text", {
+      strings: ["Développement sites Web", "Création d'identité visuelle","Stratégie de marque", "Montage vidéo"], 
+      typeSpeed: 50,  
+      backSpeed: 25,  
+      backDelay: 1000, 
+      loop: true      
+    });
+  });
+  
